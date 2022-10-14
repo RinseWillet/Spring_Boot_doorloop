a612ff76cf77aff263020d96b8133796f6aded28
RinseWillet <r.willet@gmail.com>
2022-10-14 10:29:54 +0200

    first annotations in model done_added @Table

--- src/main/java/com/itvitae/doorloopDemo/bedrijf/Bedrijf.java
+++ src/main/java/com/itvitae/doorloopDemo/bedrijf/Bedrijf.java
@@ -1,11 +1,9 @@
 package com.itvitae.doorloopDemo.bedrijf;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "bedrijf")
 public class Bedrijf {
 
     @Id
