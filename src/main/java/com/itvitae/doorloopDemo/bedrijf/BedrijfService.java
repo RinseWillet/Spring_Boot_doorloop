33bd6b6f9559e2583e89d3076835b7eadf9dac7c
RinseWillet <r.willet@gmail.com>
2022-10-14 11:30:35 +0200

    set up service layer with two methods

---
+++ src/main/java/com/itvitae/doorloopDemo/bedrijf/BedrijfService.java
@@ -0,0 +1,22 @@
+package com.itvitae.doorloopDemo.bedrijf;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BedrijfService {
+
+    @Autowired
+    BedrijfRepository bedrijfRepository;
+
+    //methods
+    public void inDeService() {
+        bedrijfRepository.save(new Bedrijf("test", 999999, 0.00));
+        System.out.println("We hebben een test bedrijf gemaakt in de service");
+    }
+
+    public Iterable<Bedrijf> getAllBedrijf() {
+        System.out.println("in service getAllBedrijf");
+        return bedrijfRepository.findAll();
+    }
+}
