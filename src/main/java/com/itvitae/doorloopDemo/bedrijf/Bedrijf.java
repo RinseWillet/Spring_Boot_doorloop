cb2f81f25474d4ddaba686849061191c5441ad66
RinseWillet <r.willet@gmail.com>
2022-10-14 10:26:11 +0200

    first annotations in model done

--- src/main/java/com/itvitae/doorloopDemo/bedrijf/Bedrijf.java
+++ src/main/java/com/itvitae/doorloopDemo/bedrijf/Bedrijf.java
@@ -1,7 +1,15 @@
 package com.itvitae.doorloopDemo.bedrijf;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Bedrijf {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     long id;
 
     String naam;
